d094f140c99ccb90c8a16dce93d219cc8b44f879
samia <151353023+ssamiaa@users.noreply.github.com>
2025-06-26 21:12:08 +0530

    get users skill

--- matcher.py
+++ matcher.py
@@ -7,3 +7,11 @@ def get_users_by_role(role):
     users = c.fetchall()
     conn.close()
     return users  # List of (id, name)
+
+def get_skills(user_id, skill_type):
+    conn = sqlite3.connect('mentorship.db')
+    c = conn.cursor()
+    c.execute("SELECT skill FROM skills WHERE user_id = ? AND type = ?", (user_id, skill_type))
+    skills = [row[0] for row in c.fetchall()]
+    conn.close()
+    return skills
